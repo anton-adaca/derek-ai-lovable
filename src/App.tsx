83dea8922bda74b29522a97c41f138482884aa73
Anton Holmes <anton.holmes@adaca.com>
2025-09-18 14:15:51 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "@/components/Layout";
+import Dashboard from "./pages/Dashboard";
+import InboxAI from "./pages/InboxAI";
+import PolicySearch from "./pages/PolicySearch";
+import MarketingStudio from "./pages/MarketingStudio";
+import LeadTools from "./pages/LeadTools";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/inbox" element={<InboxAI />} />
+            <Route path="/policy-search" element={<PolicySearch />} />
+            <Route path="/marketing" element={<MarketingStudio />} />
+            <Route path="/lead-tools" element={<LeadTools />} />
+            <Route path="/settings" element={<Settings />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
